8c06a1f044ca79885bc189a7f2a7cb73226c9221
unknown <ikanoviana49@gmail.com>
2021-12-21 20:45:19 +0700

    D3KA Laundry

---
+++ db_laundry.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2021 at 07:51 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_laundry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '01cfcd4f6b8770febfb40cb906715822');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `harga`
+--
+
+CREATE TABLE `harga` (
+  `harga_per_kilo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `harga`
+--
+
+INSERT INTO `harga` (`harga_per_kilo`) VALUES
+(6000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pakaian`
+--
+
+CREATE TABLE `pakaian` (
+  `pakaian_id` int(11) NOT NULL,
+  `pakaian_transaksi` int(11) NOT NULL,
+  `pakaian_jenis` varchar(255) NOT NULL,
+  `pakaian_jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pakaian`
+--
+
+INSERT INTO `pakaian` (`pakaian_id`, `pakaian_transaksi`, `pakaian_jenis`, `pakaian_jumlah`) VALUES
+(4, 1, 'Baju', 3),
+(5, 1, 'Celana', 3),
+(6, 1, 'Jaket', 1),
+(7, 2, 'Jaket', 2),
+(8, 2, 'Baju', 2),
+(9, 3, 'Selimut', 2),
+(10, 3, 'Celana Panjang', 3),
+(11, 3, 'Jaket', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `pelanggan_id` int(11) NOT NULL,
+  `pelanggan_nama` varchar(255) NOT NULL,
+  `pelanggan_hp` varchar(255) NOT NULL,
+  `pelanggan_alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`pelanggan_id`, `pelanggan_nama`, `pelanggan_hp`, `pelanggan_alamat`) VALUES
+(1, 'Ika', '082262727987', 'Pati'),
+(2, 'Dea', '089876789675', 'Trangkil'),
+(4, 'Desintia', '089767654565', 'Gunung Wungkal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int(11) NOT NULL,
+  `transaksi_tgl` date NOT NULL,
+  `transaksi_pelanggan` int(11) NOT NULL,
+  `transaksi_harga` int(11) NOT NULL,
+  `transaksi_berat` int(11) NOT NULL,
+  `transaksi_tgl_selesai` date NOT NULL,
+  `transaksi_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`transaksi_id`, `transaksi_tgl`, `transaksi_pelanggan`, `transaksi_harga`, `transaksi_berat`, `transaksi_tgl_selesai`, `transaksi_status`) VALUES
+(1, '2021-11-27', 1, 36000, 6, '2021-11-28', 1),
+(2, '2021-11-27', 2, 18000, 3, '2021-11-28', 0),
+(3, '2021-11-27', 4, 42000, 7, '2021-11-29', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pakaian`
+--
+ALTER TABLE `pakaian`
+  ADD PRIMARY KEY (`pakaian_id`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`pelanggan_id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pakaian`
+--
+ALTER TABLE `pakaian`
+  MODIFY `pakaian_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `pelanggan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
